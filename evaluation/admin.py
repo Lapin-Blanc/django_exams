efcedec8940e646f79fff9d90954599ac42d4f4b
Lapin-Blanc <github@lapin-blanc.com>
2014-03-14 17:49:33 +0100

    improved screenshot question

--- evaluation/admin.py
+++ evaluation/admin.py
@@ -1,6 +1,28 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
-from evaluation.models import QuestionCapture, QuestionChoixMultipleTexte, QuestionChoixMultipleImage
+from evaluation.models import QuestionCapture, ZoneImage, QuestionChoixMultipleTexte, QuestionChoixMultipleImage, QuestionCategory
 
-admin.site.register(QuestionCapture)
-admin.site.register(QuestionChoixMultipleTexte)
-admin.site.register(QuestionChoixMultipleImage)
+admin.site.register(QuestionCategory)
+
+# Question capture d'écran
+class ZoneImageInline(admin.TabularInline):
+    model = ZoneImage
+    extra = 1
+
+class QuestionCaptureAdmin(admin.ModelAdmin):
+    list_display = ['numero', 'description', 'niveau', 'categorie',]
+    list_display_links = ['numero','description']
+    list_filter = ['niveau', 'categorie__nom',]
+    inlines = [ZoneImageInline,]
+admin.site.register(QuestionCapture, QuestionCaptureAdmin)
+
+
+class QuestionChoixMultipleTexteAdmin(admin.ModelAdmin):
+    list_display = ['numero', 'niveau', 'categorie', 'description',]
+    list_filter = ['niveau', 'categorie__nom',]
+admin.site.register(QuestionChoixMultipleTexte, QuestionChoixMultipleTexteAdmin)
+
+class QuestionChoixMultipleImageAdmin(admin.ModelAdmin):
+    list_display = ['numero', 'niveau', 'categorie', 'description',]
+    list_filter = ['niveau', 'categorie__nom',]
+admin.site.register(QuestionChoixMultipleImage, QuestionChoixMultipleImageAdmin)
--- evaluation/models.py
+++ evaluation/models.py
@@ -1,16 +1,54 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
+# Partie consacrées aux questions
+#####################################
+
+# Catégories de question
+class QuestionCategory(models.Model):
+    nom = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.nom
+
+    class Meta:
+        verbose_name = u"Catégorie de question"
+        verbose_name_plural = u"Catégories de question"
+        ordering = ['nom',]
+
+# Attribue automatiquement un numéro aux questions
+def _get_next_question_num():
+    questions = Question.objects.all()
+    if questions:
+        return max([q.numero for q in questions])+1
+    else:
+        return 1
+
+# Niveau de difficulté des questions
+NIVEAU_CHOICES = (
+    (1, u'Très facile'),
+    (2, u'Facile'),
+    (3, u'Normal'),
+    (4, u'Difficile'),
+    (5, u'Très difficile'),
+)
+
 # Classe Question principale
 class Question(models.Model):
-    numero = models.IntegerField(help_text=u"Un numéro pour identifier facilement la question")
+
+    numero = models.IntegerField(help_text=u"Un numéro pour identifier facilement la question", unique=True, default=_get_next_question_num)
     description = models.CharField(max_length=100, default=u"Description courte de la question", help_text=u"Pour identifier facilement la question")
     intitule = models.TextField(default=u"Entrez le texte de la question ici...", help_text=u"Les balises HTML sont accept&eacute;es")
+    niveau = models.IntegerField(choices=NIVEAU_CHOICES, default=3)
+    categorie = models.ForeignKey(QuestionCategory)
     
     def __unicode__(self):
         question_type = getattr(self._get_subclass_question(),"question_type","")
         return u"{0:>04}[{1:<15}]: {2}".format(self.numero, question_type, self.description)
-    
+
+    class Meta:
+        ordering = ["numero",]
+
     def _get_subclass_question(self):
         from django.db.models.fields.related import SingleRelatedObjectDescriptor
         subclass_question_types = [a for a in dir(Question) if isinstance(getattr(Question, a), SingleRelatedObjectDescriptor)]
@@ -21,12 +59,42 @@ class Question(models.Model):
     def check_answer(self, *args, **kwargs):
         return self._get_subclass_question().check_answer(*args, **kwargs)
 
+# Question avec une capture d'écran où il faut indiquer sa réponse en déposant un curseur sur une image
+# si le curseur est dans l'une des bonnes zones, alors la réponse est correcte
 class QuestionCapture(Question):
     question_type = u"Capture d'écran"
+    template = "questions/question_capture.html"
+
+    # Capture specific attributes
+    image = models.ImageField(upload_to="captures")
+    
+    def check_answer(self, x, y):
+        x = int(x[0])
+        y = int(y[0])
+        for z in self.zoneimage_set.all():
+            if x>=z.x and x<=z.x+z.width and y>=z.y and y<=z.y+z.height:
+                return True
+        return False
 
     class Meta:
         verbose_name = u"Question capture d'écran"
         verbose_name_plural = u"Questions capture d'écran"
+
+# Les zone de l'image qui sont considérées comme de bonnes réponses
+class ZoneImage(models.Model):
+    x = models.IntegerField()
+    y = models.IntegerField()
+    width = models.IntegerField()
+    height = models.IntegerField()
+    question = models.ForeignKey(QuestionCapture)
+    
+    def __unicode__(self):
+        return u"{0}, {1}, {2}, {3}".format(self.x, self.y, self.width, self.height)
+
+
+
+
+
 
 class QuestionChoixMultipleTexte(Question):
     question_type = u"QCM sur texte"
